964fb160446a8810a33368b40fe66a74d88cf401
trws2 <dchen@cs.umd.edu>
2024-11-27 12:50:44 +0800

    update

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
@@ -6,21 +5,21 @@ from loguru import logger
 
 # generate a set of synthetic data using linear regression with added Gaussian noises
 np.random.seed(37)
-w = 2.0
-b = 1.0
+true_w = 2.0
+true_b = 1.0
 
-num_examples = 12000
-x = np.arange(num_examples)
-y = w * x + b
-noise = np.random.normal(0, 5, num_examples)
+num_examples = 1200
+x = np.random.rand(num_examples, 1)
+y = true_w * x + true_b
+noise = 0.1 * np.random.randn(num_examples, 1)
 y = y + noise
 
 # split them into train, val, and test
 ids = np.arange(num_examples)
 np.random.shuffle(ids)
 
-num_examples_train = 9000
-num_examples_val = 1000
+num_examples_train = 900
+num_examples_val = 100
 # num_examples_test = num_examples - num_examples_train - num_examples_val
 
 training_ids = ids[:num_examples_train]
@@ -49,4 +48,35 @@ logger.info(f"{val_y[:10]}")
 logger.info("testing x:")
 logger.info(f"{test_x[:10]}")
 logger.info(f"{test_y[:10]}")
+
+
+b = np.random.randn(1)
+w = np.random.randn(1)
+
+lr = 0.001
+
+prev_loss = None
+for iter in range(10000):
+    yhat = train_x * w + b
+    error = train_y - yhat
+    loss = (error**2).mean()
+    if prev_loss:
+        loss_diff = abs(prev_loss - loss) / max(loss, prev_loss)
+        if loss_diff < 0.0001:
+            print(
+                "loss change is too little, stop the training: prev_loss = {}, loss = {}, diff_% = {}".format(
+                    prev_loss, loss, loss_diff
+                )
+            )
+            break
+    print(f"iteration {iter}: loss = {loss}")
+
+    prev_loss = loss
+    b_grad = 2 * error.mean()
+    w_grad = 2 * (train_x * error).mean()
+
+    b = b - lr * b_grad
+    w = w - lr * w_grad
+
+print(f"Final: true_b = {true_b}, true_w = {true_w}, b = {b}, w = {w}, loss = {loss}")
 
